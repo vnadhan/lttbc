912ba48e7ee20c5908716b1bef3e1363d4218ed2
Vishnu Nadhan <nadhan.vishnu@gmail.com>
2023-04-23 22:10:09 +0200

    Convert setup.py to project.toml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
 import os.path as osp
 import sys
 
+import numpy as np
 from setuptools import Extension, setup
 
 
@@ -13,45 +13,14 @@ def read(*parts):
     return open(osp.join(get_script_path(), *parts)).read()
 
 
-class numpy_get_include:
-    def __str__(self):
-        import numpy
-        return numpy.get_include()
-
-
-lttbc_py = Extension("lttbc", sources=["lttbc.c"],
-                     define_macros=[
-                         ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-                     include_dirs=[numpy_get_include(),
-                                   get_script_path()],
-                     )
-
-setup(
-    name="lttbc",
-    author="European XFEL GmbH",
-    use_scm_version=True,
-    ext_modules=[lttbc_py],
-    author_email="dennis.goeries@xfel.eu",
-    maintainer="Dennis Goeries",
-    url="https://github.com/dgoeries/lttbc/",
-    description="Largest triangle three buckets module for Python written in C",
-    long_description=read("README.txt"),
-    long_description_content_type="text/markdown",
-    license="MIT",
-    install_requires=["numpy"],
-    setup_requires=["setuptools_scm"],
-    python_requires=">=3.5",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
+setup_args = dict(
+    ext_modules=[
+        Extension(
+            "lttbc",
+            sources=["lttbc.c"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            include_dirs=[np.get_include(), get_script_path()],
+        )
+    ]
 )
+setup(**setup_args)
